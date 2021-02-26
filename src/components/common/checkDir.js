f6e12d6a9eab6ea85ddd0c2c80a095bb94ea856a
artalatarta <artur@mucowski.pl>
2021-02-26 22:42:30 +0100

    deleted console logs in check dir

--- src/components/common/checkDir.js
+++ src/components/common/checkDir.js
@@ -2,9 +2,6 @@ const { existsSync } = require('fs');
 const { APP_DIR, SERVER_DIR, TOOLS_DIR } = require('./paths');
 
 exports.checkDir = function () {
-	console.log(APP_DIR());
-	console.log(SERVER_DIR());
-	console.log(TOOLS_DIR());
 	if (!existsSync(TOOLS_DIR()) || !existsSync(APP_DIR()) || !existsSync(SERVER_DIR())) {
 		const chalk = require('chalk');
 
