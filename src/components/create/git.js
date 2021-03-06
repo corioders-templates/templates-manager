a1580f69e5970f09f6adc82f846410841e41bace
artalatarta <artur@mucowski.pl>
2021-03-06 23:57:34 +0100

    turn on husky before commiting

--- src/components/create/git.js
+++ src/components/create/git.js
@@ -1,5 +1,5 @@
-const { spawn } = require('../common/spawn');
-const { PROJECT_DIR } = require('../common/paths');
+const { spawn } = require('../../util/spawn');
+const { PROJECT_DIR, MAIN_REPO, TOOLS_DIR } = require('../common/paths');
 
 exports.clone = async function (selectedUrl, name) {
 	try {
@@ -11,11 +11,13 @@ exports.clone = async function (selectedUrl, name) {
 
 exports.git = async function (url, name) {
 	try {
+		await spawn('yarn', ['install'], { cwd: TOOLS_DIR(name) });
 		await spawn('git', ['add', '.'], { cwd: PROJECT_DIR(name) });
-		await spawn('git', ['commit', '-m', 'init'], { cwd: PROJECT_DIR(name) });
+		await spawn('git', ['commit', '-m', 'init', '--no-verify'], { cwd: PROJECT_DIR(name) });
 		await spawn('git', ['remote', 'rename', 'origin', 'template'], { cwd: PROJECT_DIR(name) });
 		await spawn('git', ['remote', 'set-url', '--push', 'template', 'no_push'], { cwd: PROJECT_DIR(name) });
 		await spawn('git', ['remote', 'add', 'origin', url], { cwd: PROJECT_DIR(name) });
+		await spawn('git', ['push', 'origin', 'master', '--no-verify'], { cwd: PROJECT_DIR(name) });
 	} catch (error) {
 		console.error(error);
 	}
