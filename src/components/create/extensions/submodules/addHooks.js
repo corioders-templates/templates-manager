fe3bcbce4dce39c8eee2452e448e50285c9cc4bd
artalatarta <artur@mucowski.pl>
2021-03-11 18:00:21 +0100

    moved package json for husky to hooks dir

--- src/components/create/extensions/submodules/addHooks.js
+++ src/components/create/extensions/submodules/addHooks.js
@@ -6,14 +6,13 @@ const { writeFile } = require('fs/promises');
 const { existsSync } = require('fs');
 
 exports.addHooks = async function (submoduleName, projectName) {
-	const JSON_PATH = resolve(SUBMODULE(submoduleName, projectName), 'package.json');
+	const HOOKS_PATH = resolve(SUBMODULE(submoduleName, projectName), 'hooks');
+	const JSON_PATH = resolve(HOOKS_PATH, 'package.json');
+	await copyDir(resolve(MAIN_REPO(projectName), 'tools', 'config'), resolve(HOOKS_PATH));
 	if (!existsSync(JSON_PATH)) await writeFile(JSON_PATH, '{}', 'utf-8');
-	await spawn('npx', ['add-dependencies', '@commitlint/cli', 'commitlint-config-jira', 'commitlint-plugin-jira-rules', '--dev'], {
-		cwd: SUBMODULE(submoduleName, projectName),
-	});
+	await spawn('npx', ['add-dependencies', 'husky', '@commitlint/cli', 'commitlint-config-jira', 'commitlint-plugin-jira-rules', '--dev'], { cwd: HOOKS_PATH });
 	const json = require(JSON_PATH);
-	if (json.scripts == undefined) json.scripts = { postinstall: 'husky install ./hooks/.husky' };
-	else json.scripts.postinstall = 'husky install ./hooks/.husky';
-	await copyDir(resolve(MAIN_REPO(projectName), 'tools', 'config'), resolve(SUBMODULE(submoduleName, projectName), 'hooks'));
+	if (json.scripts == undefined) json.scripts = { postinstall: 'husky install ./.husky' };
+	else json.scripts.postinstall = 'husky install ./.husky';
 	await writeFile(JSON_PATH, JSON.stringify(json, null, 2), 'utf-8');
 };
