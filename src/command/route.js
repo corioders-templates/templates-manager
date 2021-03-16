2aef77a90c90e540c7c5837ce8905da983c1688d
artalatarta <artur@mucowski.pl>
2021-03-16 16:13:32 +0100

    component dir -> route dir

--- src/command/route.js
+++ src/command/route.js
@@ -1,5 +1,5 @@
-const { createRoute } = require('../components/component/createRoute');
-const { prepareName } = require('../components/component/prepareName');
+const { createRoute } = require('../components/route/createRoute');
+const { prepareName } = require('../components/route/prepareName');
 
 exports.route = async function (componentName) {
 	const { name, dirName, alias } = prepareName(componentName);
