a9780597aeee384e36707f72988b2f84d784998a
artalatarta <artur@mucowski.pl>
2021-03-17 16:09:07 +0100

    add webpack alias

--- src/command/route.js
+++ src/command/route.js
@@ -1,7 +1,9 @@
+const { addAlias } = require('../components/route/addAlias');
 const { createRoute } = require('../components/route/createRoute/createRoute');
 const { prepareName } = require('../components/route/prepareName');
 
 exports.route = async function (componentName) {
 	const { name, dirName, alias } = prepareName(componentName);
 	await createRoute(name, dirName);
+	await addAlias(alias, dirName);
 };
