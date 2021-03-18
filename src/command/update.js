0911a3cacc6009e103792e7c1dd32f1858cbae39
artalatarta <artur@mucowski.pl>
2021-03-18 16:12:17 +0100

    deleted check dir in update command

--- src/command/update.js
+++ src/command/update.js
@@ -3,8 +3,6 @@ const { checkDir } = require('../util/fileSystem/checkDir');
 const { PROJECT_DIR } = require('../components/common/paths');
 
 exports.update = function () {
-	if (!checkDir()) return;
-
 	let { status: add } = spawnSync('git', ['pull', 'template'], { stdio: 'inherit', cwd: PROJECT_DIR() });
 	if (add != 0) return;
 };
