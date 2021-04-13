5c26ce2dbf54d4855e8c19141ab952e0d2fcb251
watjurk <watjurk@gmail.com>
2021-04-14 00:06:53 +0200

    CLI-24: added git-remote-provider

--- src/lib/plugins/git-remote-provider/index.ts
+++ src/lib/plugins/git-remote-provider/index.ts
@@ -1,38 +1,21 @@
+import GithubProvider from './githubProvider';
 import { Config } from '@/lib/config';
-import { writeFile } from 'fs/promises';
-import { resolve } from 'path';
 
 export enum ProviderType {
 	github,
 }
 
 export default class GitRemoteProvider {
-	private providerType;
+	private providerType: ProviderType;
 	constructor(providerType: ProviderType) {
 		this.providerType = providerType;
 	}
 
 	apply(dataFolder: string, config: Config): void {
+		switch (this.providerType) {
+			case ProviderType.github:
+				config['git-remote-provider'] = new GithubProvider(dataFolder);
+		}
 		// provider type has only one option, for now
-		config['git-remote-provider'] = new GithubProvider(dataFolder);
 	}
-}
-
-const tokenFile = 'token';
-
-class GithubProvider {
-	dataFolder: string;
-	constructor(dataFolder: string) {
-		this.dataFolder = dataFolder;
-	}
-
-	async login(token: string): Promise<void> {
-		writeFile(resolve(this.dataFolder, tokenFile), token);
-	}
-
-	async createRepo(organizationName: string, repoName: string): Promise<void> {}
-	async isLogged(): Promise<boolean> {
-		return false;
-	}
-	async signout(): Promise<void> {}
 }
