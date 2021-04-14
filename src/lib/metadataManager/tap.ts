1f3e7dd81780300317bbcbb5d4abaa9b1570d727
watjurk <watjurk@gmail.com>
2021-04-15 00:17:16 +0200

    CLI-35: reviewed taps code, and added some todos into the code

--- src/lib/metadataManager/tap.ts
+++ src/lib/metadataManager/tap.ts
@@ -1,12 +1,11 @@
+import { metadata } from '@/lib/constant/location/metadata';
+import { rmdir, lstat, readdir } from 'fs/promises';
 import { resolve, join } from 'path';
 import simpleGit from 'simple-git';
 
-import { metadata } from '@/lib/constant/location/metadata';
-
-import { rmdir, lstat, readdir } from 'fs/promises';
-
 export async function tap(url: string): Promise<void> {
 	const git = simpleGit();
+	// TODO: move this `resolve(metadata, url.split('//')[1])` into function because this repeats in untap
 	await git.clone(url, resolve(metadata, url.split('//')[1]));
 }
 
@@ -25,10 +24,11 @@ async function removeEmptyDirectories(directory: string): Promise<void> {
 		return;
 	}
 	let fileNames = await readdir(directory);
+	// TODO: swap logic around if( fileNames.length === 0) then: await rmdir(directory), and return
 	if (fileNames.length > 0) {
 		const recursiveRemovalPromises = fileNames.map((fileName) => removeEmptyDirectories(join(directory, fileName)));
 		await Promise.all(recursiveRemovalPromises);
-		fileNames = await readdir(directory);
+		fileNames = await readdir(directory); //? what is the purpure of this line?
 	}
 	if (fileNames.length === 0) await rmdir(directory);
 }
