4771f8bd64faceb594b785b0657a5df778616479
watjurk <watjurk@gmail.com>
2021-04-15 00:22:39 +0200

    CLI-39: moved projectPath to function and removed some unnecessary typings

--- src/lib/coriodersManager/project.ts
+++ src/lib/coriodersManager/project.ts
@@ -1,20 +1,18 @@
-import { resolve } from 'path';
-import simpleGit, { SimpleGit } from 'simple-git';
-
 import { storage, pathsJsonFilename } from '@/lib/constant/location/project';
 import { exists } from '@/nodekit/fs';
-
 import { mkdir, unlink, writeFile } from 'fs/promises';
+import { resolve } from 'path';
+import simpleGit from 'simple-git';
 
 export async function create(name: string, url: string): Promise<void> {
 	const exists = await projectExists(name);
 	if (exists) throw new Error(`Project with name ${name} already exists`);
-	const projectPath = resolve(storage, name);
+	const projectPath = getProjectPath(name);
 
 	await mkdir(projectPath, { recursive: true });
 	await writeFile(resolve(projectPath, pathsJsonFilename), '{}');
 
-	const git: SimpleGit = simpleGit(projectPath);
+	const git = simpleGit(projectPath);
 	await git.init();
 	await git.addRemote('origin', url);
 }
@@ -23,9 +21,13 @@ export async function remove(name: string): Promise<void> {
 	const exists = await projectExists(name);
 	if (!exists) throw new Error(`Project with name ${name} doesn't exist`);
 
-	await unlink(resolve(storage, name));
+	await unlink(getProjectPath(name));
 }
 
 async function projectExists(name: string): Promise<boolean> {
-	return await exists(resolve(storage, name));
+	return await exists(getProjectPath(name));
+}
+
+function getProjectPath(projectName: string): string {
+	return resolve(storage, projectName);
 }
