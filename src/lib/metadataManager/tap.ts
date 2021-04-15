899c5fccf6d0052dd99654c5d853fb12af37d10d
artalatarta <artur@mucowski.pl>
2021-04-15 15:04:51 +0200

    CLI-41: taps: move parsing url to a function

--- src/lib/metadataManager/tap.ts
+++ src/lib/metadataManager/tap.ts
@@ -1,21 +1,26 @@
-import { metadata } from '@/lib/constant/location/metadata';
-import { rmdir, lstat, readdir } from 'fs/promises';
 import { resolve, join } from 'path';
 import simpleGit from 'simple-git';
 
+import { metadata } from '@/lib/constant/location/metadata';
+
+import { rmdir, lstat, readdir } from 'fs/promises';
+
 export async function tap(url: string): Promise<void> {
 	const git = simpleGit();
-	// TODO: move this `resolve(metadata, url.split('//')[1])` into function because this repeats in untap
-	await git.clone(url, resolve(metadata, url.split('//')[1]));
+	await git.clone(url, parseUrl(url));
 }
 
 export async function untap(url: string): Promise<void> {
-	await rmdir(resolve(metadata, url.split('//')[1]), { recursive: true });
+	await rmdir(resolve(metadata, parseUrl(url)), { recursive: true });
 	await removeEmptyDirectories(metadata);
 }
 
 export function getTaps(): string[] {
 	return [];
+}
+
+function parseUrl(url: string): string {
+	return resolve(metadata, url.split('//')[1]);
 }
 
 async function removeEmptyDirectories(directory: string): Promise<void> {
@@ -24,11 +29,10 @@ async function removeEmptyDirectories(directory: string): Promise<void> {
 		return;
 	}
 	let fileNames = await readdir(directory);
-	// TODO: swap logic around if( fileNames.length === 0) then: await rmdir(directory), and return
 	if (fileNames.length > 0) {
 		const recursiveRemovalPromises = fileNames.map((fileName) => removeEmptyDirectories(join(directory, fileName)));
 		await Promise.all(recursiveRemovalPromises);
-		fileNames = await readdir(directory); //? what is the purpure of this line?
+		fileNames = await readdir(directory);
 	}
 	if (fileNames.length === 0) await rmdir(directory);
 }
