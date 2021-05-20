f919da5d4be0a1881f3a3d4c16cd88e34972739f
artalatarta <artur@mucowski.pl>
2021-05-20 12:10:05 +0200

    CPM-125: class corioders manager

--- src/lib/manager/index.ts
+++ src/lib/manager/index.ts
@@ -1,5 +1,8 @@
 import { defaultModulesFolder } from '@/lib/constant/location/modules';
+import { defaultPathsJsonFilename, defaultStoragePath } from '@/lib/constant/location/project';
 
+import { CoriodersManager } from './corioders';
 import { ModulesManager } from './modules';
 
+export const defaultCoriodersManager = new CoriodersManager(defaultStoragePath, defaultPathsJsonFilename);
 export const defaultModulesManager = new ModulesManager(defaultModulesFolder);
