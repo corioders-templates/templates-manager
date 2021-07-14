1f0db3aba7c9c64505616db4c008318c2fcab9ee
watjurk <watjurk@gmail.com>
2021-07-15 00:29:26 +0200

    CPM-190: added CliApi as property in GlobalPluginsObject

--- src/plugins/global.ts
+++ src/plugins/global.ts
@@ -1,17 +1,26 @@
 import { AsyncSeriesHook } from 'tapable';
 
+import { CliInterface } from '@/cli/api';
+import * as cliDefaultApi from '@/cli/defaultApi';
+
 import * as gitRemote from './git-remote-provider';
 
 export interface GlobalPluginsObject {
+	'cli-api': CliInterface;
+
 	'git-remote-provider': gitRemote.Provider;
 	hooks: GlobalHooks;
 }
 
 class GlobalClass implements GlobalPluginsObject {
+	'cli-api': CliInterface;
+
 	'git-remote-provider': gitRemote.Provider;
 	hooks: GlobalHooks;
 
 	constructor() {
+		this['cli-api'] = cliDefaultApi.cliApi;
+
 		this['git-remote-provider'] = new gitRemote.NoopProvider();
 		this.hooks = new GlobalHooks();
 	}
