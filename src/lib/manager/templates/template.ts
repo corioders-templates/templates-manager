22776564f4bdb23ce625b4872d311422f02ef22a
watjurk <watjurk@gmail.com>
2021-07-15 00:21:20 +0200

    CPM-189: replaced placeholder with globalPluginsObject

--- src/lib/manager/templates/template.ts
+++ src/lib/manager/templates/template.ts
@@ -5,7 +5,7 @@ import { CliInterface } from '@/cli/api';
 import { cliApi } from '@/cli/defaultApi';
 import { ModulesManager } from '@/lib/manager/modules';
 import { ProgramManager } from '@/lib/manager/program';
-import { GlobalPluginsObject } from '@/plugins/global';
+import { globalPluginsObject } from '@/plugins/global';
 import { templatesApi } from '@/templates';
 
 import { TemplatesApi } from './api';
@@ -25,10 +25,9 @@ export class Template {
 
 	async executeTemplate(): Promise<Folder> {
 		const templateFolder = await this.templateFolderPromise;
-		const pluginGlobalObjectPlaceholder = {} as GlobalPluginsObject;
 
 		const tfo: TemplateFunctionObject = {
-			templatesApi: new TemplatesApi(templateFolder, pluginGlobalObjectPlaceholder),
+			templatesApi: new TemplatesApi(templateFolder, globalPluginsObject),
 
 			// TODO(@watjurk): how to choose cli api !? Maybe provide this in plugins Global object ?
 			cliApi: cliApi,
