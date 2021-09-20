c98e42ef6107c85b79e1326e433589e96a05704e
watjurk <watjurk@gmail.com>
2021-09-20 20:06:16 +0200

    Merge branch 'master' into CTM-208-refactor-manager

--- src/plugin/global.ts
+++ src/plugin/global.ts
@@ -3,25 +3,21 @@ import { AsyncSeriesHook } from 'tapable';
 import { cliInterface } from '@/cli/api';
 import * as cliDefaultApi from '@/cli/defaultApi';
 
-import * as gitRemote from './git-remote-provider';
 
 export interface GlobalPluginsObject {
 	'cli-api': cliInterface;
 
-	'git-remote-provider': gitRemote.Provider;
 	hooks: GlobalHooks;
 }
 
 class GlobalClass implements GlobalPluginsObject {
 	'cli-api': cliInterface;
 
-	'git-remote-provider': gitRemote.Provider;
 	hooks: GlobalHooks;
 
 	constructor() {
 		this['cli-api'] = new cliDefaultApi.CliApi();
 
-		this['git-remote-provider'] = new gitRemote.NoopProvider();
 		this.hooks = new GlobalHooks();
 	}
 }
