ce6db55076bea35b8106c07ca98fe596fef275ac
watjurk <watjurk@gmail.com>
2021-11-18 23:12:36 +0100

    CTM-224: Add exception for 'local.' prefix in importPath
    When user wants to develop template locally we need to give him a way
    to run local templates that what 'local.' prefix is for.

--- src/lib/manager/module/importPath.ts
+++ src/lib/manager/module/importPath.ts
@@ -3,6 +3,15 @@ import { resolve } from 'path';
 import { download } from './download';
 
 export async function importPathToAbsolute(importPath: string, downloadFolderPath: string): Promise<string> {
+	importPath = importPath.trim();
+
+	// Exception for local paths.
+	if (importPath.startsWith('local.')) {
+		const localImportPath = importPath.replace('local.', '');
+		if (localImportPath !== '') return localImportPath;
+		return process.cwd();
+	}
+
 	validateImportPath(importPath);
 	await download(importPath, downloadFolderPath);
 	return resolve(downloadFolderPath, importPath);
